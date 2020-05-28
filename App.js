d69becae89d914613b3a8ed110fa95cace9fdfa3
Apianda <andres.pianda00@usc.edu.co>
2020-05-28 17:16:09 -0500

    ante no guardado

--- App.js
+++ App.js
@@ -1,19 +1,98 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component} from 'react';
+import { StyleSheet,  View, Image, Button, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+
+
+//npm install expo-font
+class Notas extends Component {
+  render() {
+
+    return (
+      
+      <View style={styles.container}>
+
+        <Image source={require('./assets/image/logoMega.png')} style={styles.imag} />
+      
+
+      
+        <View style={styles.ob}>
+          <TextInput
+            placeholder='Usuario'
+            placeholderTextColor='#88B053'
+            maxLength={8}
+            style={styles.bordes}>
+
+
+          </TextInput>
+        
+
+     
+          <TextInput
+            placeholder='passwod'
+            placeholderTextColor='#88B053'
+            maxLength={8}
+            style={styles.bordes}
+            type='password' >
+          </TextInput>
+        
+
+
+
+        
+          <Button title='Ingresar' color='#88B053' />
+        </View>
+
+       
+        </View>
+      
+
+    )
+  }
 }
 
-const styles = StyleSheet.create({
+const styles =StyleSheet.create  ({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    //backgroundColor: '#FAFAFA',
+    backgroundColor: 'gray',
+    justifyContent:'center',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingLeft: 10,
+    paddingRight: 10
+    
+
+
   },
-});
+  ob:{
+  
+    backgroundColor: '#FAFAFA',
+ 
+
+  },
+  bordes:{
+    borderWidth: 1,
+    borderColor: '#88B053',
+    padding: 10,
+    marginTop: 10,
+    paddingRight: 10,
+    paddingLeft: 10
+    
+  },
+  imag:{
+      width: 180, 
+      height: 180,
+
+  },
+
+  titulos: {
+
+    fontFamily: 'Helvetica',
+    fontSize: 25
+    
+  }
+
+})
+
+
+export default Notas;
